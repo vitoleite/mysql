9dbdb2b33a6a6b5e5e8d179b8d0657aafff00494
Vitor Leite <vitudhr@gmail.com>
2021-11-28 19:00:41 -0300

    Atualizando joins

---
+++ join_CursoEmVideo.sql
@@ -0,0 +1,29 @@
+use cadastro;
+show tables;
+describe assiste_curso;
+
+create table assiste_curso (
+	id int not null auto_increment,
+    Data_assistida date,
+    idgafanhoto int,
+    idcurso int,
+    primary key (id),
+    foreign key (idgafanhoto) references gafanhotos(id),
+    foreign key (idcurso) references cursos(idcurso)
+) default charset = utf8;
+
+insert into assiste_curso values (default, '2021-11-28', '1', '2');
+
+describe gafanhotos;
+
+select g.nome, c.nome, c.descricao, ac.data from gafanhotos as g
+inner join cursos as c on g.cursopreferido = c.idcurso
+inner join assiste_curso as ac on g.id = ac.idgafanhoto
+;
+
+select * from assiste_curso;
+
+select g.id, g.nome, c.nome, ac.data, ac.idgafanhoto from gafanhotos g
+inner join assiste_curso ac on g.id = ac.idgafanhoto
+inner join cursos c on ac.idcurso = c.idcurso
+order by g.nome;
---
+++ left_join.sql
@@ -0,0 +1,5 @@
+-- Vai mostrar todos os dados da tabela da esquerda, enquanto a tabela de conexão (direita) poderá vir com valores nulos ou vazios.
+
+select g.nome, g.cursopreferido, c.nome from gafanhotos g
+left join cursos c on g.cursopreferido = c.idcurso
+order by g.nome;
