b1cd4872d74a746c5f9cdf9307d4ac67b335adcd
Vitor Leite <vitudhr@gmail.com>
2021-12-09 08:10:34 -0300

    Atualizando conceitos de GroupBy

--- groupBy.sql
+++ groupBy.sql
@@ -7,3 +7,13 @@ select nome, ano, totaulas, sum(totaulas) as total_aulas_ano
 from cursos
 group by ano
 order by total_aulas_ano;
+
+select * from cursos;
+
+select c.nome, c.ano, c.totaulas,
+case
+	when not c.ano = 2010 then sum(c.totaulas)
+    else c.ano
+end as Total_aulas_por_ano_maior_2010
+from cursos as c
+group by c.ano;
