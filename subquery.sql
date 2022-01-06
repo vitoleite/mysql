f290c348ccd6902b817aa97b7b3236be5ac19aac
Vitor Leite <vitudhr@gmail.com>
2022-01-06 13:35:02 -0300

    Atualizando conceitos de subqueries

--- subquery.sql
+++ subquery.sql
@@ -6,13 +6,15 @@ mas nas subqueries vc pode explorar melhor os filtros ainda mais quando se trata
 
 use cadastro;
 
-select * from gafanhotos;
+select * from assiste_curso;
 
 select g.nome, c.ano from gafanhotos as g
 join cursos as c on c.idcurso = g.cursopreferido
 where c.ano = 2021;
 
-select g.nome from gafanhotos g
+select g.nome, c.ano, ac.data from gafanhotos g
+join assiste_curso as ac on ac.idgafanhoto = g.id
+join cursos as c on c.idcurso = g.cursopreferido
 where g.cursopreferido in (
 				select g.cursopreferido from cursos c where g.cursopreferido = c.idcurso and c.ano = 2021)
 order by g.nome;
