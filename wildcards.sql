245aa9dfbd328342455cfd908991e6a63e6cba6d
Vitor Leite <vitudhr@gmail.com>
2022-01-09 17:30:10 -0300

    Conceito de Wildcards para auxiliar nos filtros

---
+++ wildcards.sql
@@ -0,0 +1,5 @@
+-- A wildcard character is used to substitute one or more characters in a string.
+
+-- Symbol		Description								Example
+-- %			Represents zero or more characters		bl% finds bl, black, blue, and blob
+-- _			Represents a single character			h_t finds hot, hat, and hit
