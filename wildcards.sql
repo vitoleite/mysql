cc374f86d65a0714d5cd55a18020c1105154283e
Vitor Leite <vitudhr@gmail.com>
2022-01-09 17:39:35 -0300

    Incrementando conceitos de Wildcards

--- wildcards.sql
+++ wildcards.sql
@@ -3,3 +3,9 @@
 -- Symbol		Description								Example
 -- %			Represents zero or more characters		bl% finds bl, black, blue, and blob
 -- _			Represents a single character			h_t finds hot, hat, and hit
+
+select * from gafanhotos as gf
+where gf.nome like '%Silva';
+
+select * from gafanhotos as gf
+where gf.profissao like 'At__';
