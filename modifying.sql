4affe7804dd9f5ed829401d8e738e2bdd0673f66
Vitor Leite <75452535+vitoleite@users.noreply.github.com>
2022-02-04 08:26:58 -0300

    Removendo código desnecessário

--- modifying.sql
+++ modifying.sql
@@ -1,22 +1,13 @@
 -- Alterando a estrutura e definindo como nao vazia, importante usar o default '' pois a sintaxe do sql nao permitira sem.
-describe pessoas;
-
 alter table pessoas
 modify column profissao varchar(20) not null default '';
 
 
-
 -- Renaming column
-describe pessoas;
-
+-- Nesse caso a sequencia é: Velho > novo.
 alter table pessoas
 change column profissao prof varchar(20);
-
--- Nesse caso a sequencia é: Velho > novo.
-
 
 -- Renaming tables
 alter table pessoas
 rename to pessoinhas;
-
-describe pessoinhas;
