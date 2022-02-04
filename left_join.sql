bc838db578326c3c79cac78fb08d69ebbf69638e
Vitor Leite <75452535+vitoleite@users.noreply.github.com>
2022-02-04 08:28:03 -0300

    Atualizando comentário

--- left_join.sql
+++ left_join.sql
@@ -1,5 +1,9 @@
--- Vai mostrar todos os dados da tabela da esquerda, enquanto a tabela de conexão (direita) poderá vir com valores nulos ou vazios.
+/*
+Vai mostrar todos os dados da tabela da esquerda, enquanto a tabela de conexão (direita)
+poderá vir com valores nulos ou vazios.
+*/
 
-select g.nome, g.cursopreferido, c.nome from gafanhotos g
+select g.nome, g.cursopreferido, c.nome
+from gafanhotos g
 left join cursos c on g.cursopreferido = c.idcurso
 order by g.nome;
